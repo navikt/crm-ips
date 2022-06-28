ddbef71f14d7a578c37f32a22163e1e6e4fb146f
trineronning <trine.ronning@soprasteria.com>
2022-06-29 00:48:44 +0200

    ipsuo15,1

--- scripts/newScratchOrg.bat
+++ scripts/newScratchOrg.bat
@@ -1,30 +1,32 @@
 echo "Oppretter scratch org"
-call sfdx force:org:create -f config\project-scratch-def.json --setalias %1 --durationdays %2 --setdefaultusername --json --loglevel fatal  --wait 10
+call sfdx force:org:create -f config\project-scratch-def.json --setalias Ronning68 --durationdays 20 --setdefaultusername --json --loglevel fatal  --wait 10
 
-echo "Installerer crm-platform-base ver. 0.153"
-call sfdx force:package:install --package 04t7U000000TpC2QAK -r -k %3 --wait 10 --publishwait 10
+echo "Installerer crm-platform-base ver. 0.157"
+call sfdx force:package:install --package 04t7U000000TpVEQA0 -r -k %3 --wait 10 --publishwait 10
 
-echo "Installerer crm-platform-integration ver. 0.78"
-call sfdx force:package:install --package 04t7U000000Tp4DQAS -r -k %3 --wait 10 --publishwait 10
+echo "Installerer crm-platform-integration ver. 0.79"
+call sfdx force:package:install --package 04t7U000000TpVTQA0 -r -k %3 --wait 10 --publishwait 10
 
-echo "Installerer crm-platform-access-control ver. 0.94"
-call sfdx force:package:install --package 04t7U000000TpJrQAK -r -k %3 --wait 10 --publishwait 10
+echo "Installerer crm-platform-access-control ver. 0.95"
+call sfdx force:package:install --package 04t7U000000TpVTQA0 -r -k %3 --wait 10 --publishwait 10
 
-echo "Installerer crm-shared-timeline ver. 1.14"
-call sfdx force:package:install --package 04t7U000000TpG4QAK -r -k %3 --wait 10 --publishwait 10
+echo "Installerer crm-shared-timeline ver. 1.15"
+call sfdx force:package:install --package 04t7U000000TpOcQAK -r -k %3 --wait 10 --publishwait 10
 
 echo "Installerer crm-shared-base ver. 1.1"
 call sfdx force:package:install --package 04t2o000000ySqpAAE -r -k %3 --wait 10 --publishwait 10
 
-
 echo "Installerer crm-arbeidsgiver-base ver. 1.234"
 call sfdx force:package:install --package 04t7U000000TpGOQA0 -r -k %3 --wait 10 --publishwait 10
 
-echo "Installerer crm-community-base ver. 0.63"
-call sfdx force:package:install --package 04t7U000000Tp7WQAS -r -k %3 --wait 10 --publishwait 10
+echo "Installerer crm-community-base ver. 0.64"
+call sfdx force:package:install --package 04t7U000000TpSZQA0 -r -k %3 --wait 10 --publishwait 10
 
-echo "Installerer crm-journal-utilities ver. 0.13.0"
-call sfdx force:package:install --package 04t7U000000ToHfQAK -r -k %3 --wait 10 --publishwait 10
+echo "Installerer crm-journal-utilities ver. 0.15.0"
+call sfdx force:package:install --package 04t7U000000TpOhQAK -r -k %3 --wait 10 --publishwait 10
+
+echo "Installerer crm-ips ver. 0.208.0"
+call sfdx force:package:install --package 04t7U000000TptVQAS -r -k %3 --wait 10 --publishwait 10
 
 echo "Dytter kildekoden til scratch org'en"
 call sfdx force:source:push
