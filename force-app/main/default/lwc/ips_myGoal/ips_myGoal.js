730505ee0a170b36ed58aab38a44a43c1e5bd61e
annolse <ann.kristin.olsen@nav.no>
2022-09-10 17:31:49 +0200

    Goals

--- force-app/main/default/lwc/ips_myGoal/ips_myGoal.js
+++ force-app/main/default/lwc/ips_myGoal/ips_myGoal.js
@@ -1,20 +1,27 @@
-import { LightningElement,api,wire } from 'lwc';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import MAIN_GOAL from '@salesforce/schema/Work_Trail__c.ips_Main_goal__c';
-
-const fields = [MAIN_GOAL];
+import { LightningElement,wire,api} from 'lwc';
+import getUserGoal from '@salesforce/apex/IPS_myWorkTrailController.getUserGoal';
 
 /* all logos related to IPS/UO portal */
 import IPS_HOME_LOGOS from '@salesforce/resourceUrl/ips_home_logo';
 
 export default class Ips_myGoal extends LightningElement {
-
+    /* profile logo */
+    myGoalImg = IPS_HOME_LOGOS + '/DirectionSignFilled.svg';
     @api recordId;
-    /*fetch name from logged in user */
-    @wire(getRecord, { recordId: '$recordId', fields })
-    Work_Trail__c;
-    
-    get goal() {
-        return getFieldValue(this.Work_Trail__c.data, MAIN_GOAL);
-    }  
+    wiredRequestResult;
+    mainGoal;
+    isData = false;
+
+    @wire(getUserGoal)
+    async wiredRequest(result){
+        this.wiredRequestResult = result;
+        if(result.data){
+            this.wiredRequestResult = result.data;
+            this.mainGoal = this.wiredRequestResult.fields.ips_Main_goal__c.value;
+            this.error = undefined;
+            this.isData = true;
+        }else if(result.error) {
+            this.error = result.error;
+        }
+    }
 }
