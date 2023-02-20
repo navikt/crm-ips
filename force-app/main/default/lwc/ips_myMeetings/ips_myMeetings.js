9d38dc0d1a847a9ffdf6c439462484e7bf7e912c
annolse <ann.kristin.olsen@nav.no>
2023-02-20 22:55:58 +0100

    LWC - meetings

--- force-app/main/default/lwc/ips_myMeetings/ips_myMeetings.js
+++ force-app/main/default/lwc/ips_myMeetings/ips_myMeetings.js
@@ -3,59 +3,17 @@ import { NavigationMixin } from 'lightning/navigation';
 import Id from '@salesforce/user/Id';
 import getUserWorkTrailId from '@salesforce/apex/IPS_myWorkTrailController.getUserWorkTrailId';
 import getAllActivity from '@salesforce/apex/IPS_myActivityController.getAllActivity';
-
-const COLUMNS =[
-    {
-        label: 'Møtedato', 
-        fieldName: 'ActivityDate',
-        type: 'date',
-        hideDefaultActions: true,
-            typeAttributes:{
-            year:"numeric" ,
-            month:"numeric" ,
-            day:"2-digit" ,
-            weekday:"long"
-        }
-    },
-    {label: 'Emne', fieldName: 'Subject', type: 'text', hideDefaultActions: true},
-    {
-        type: 'button',
-        fixedWidth: 170,
-        typeAttributes:{
-            label: 'Se detaljer',
-            title: 'Se detaljer',
-            name: 'Møte',
-            variant: 'Brand'
-        }
-    },
-    {
-        type:'button',
-        fixedWidth: 170,
-        typeAttributes:{
-            label: 'Fullført',
-            title: 'Fullført',
-            name: 'IPS_Status1__c',
-            variant: 'Destructive',
-            disabled:{fieldName:'disableButton'}
-        }
-    }
-]
+/* all logos related to IPS/UO portal */
+import IPS_HOME_LOGOS from '@salesforce/resourceUrl/ips_home_logo';
 
 export default class Ips_myMeetings extends NavigationMixin(LightningElement) {
+myMeetingImg = IPS_HOME_LOGOS + '/CalenderFilled.svg';
 currentUser = Id;
-//currentUser ='0051X00000DtVvmQAF';
-activityRecords;
-@track activityRecord
+//currentUser ='0053O000007R0NUQA0';
+@track activityRecords;
 @track record;
 recordIds;
 isActivity = false;
-columns = COLUMNS;
-isloading = true;
-
-
-get isMobile() {
-    return window.screen.width < 576;
-  }
 
 /* Fetch recordId from logged in user */
 @wire(getUserWorkTrailId,{userId: '$currentUser'})
@@ -63,39 +21,24 @@ wiredtrail({ error, data }) {
         if (data) {
             this.record = data[0];
             this.recordIds = this.record?.Id;
+            this.contactId = this.record?.ips_Participant__r.PersonContactId;
         } else if (error) {
             console.log('Something went wrong:', error);
         }
     }
 
-    @wire(getAllActivity, {workTrailId:'$recordIds'})
+    @wire(getAllActivity, {workTrailId:'$recordIds',contactId: '$contactId'})
     userActivity({error,data}){
        if(data){
         if(data.length>0){
             this.activityRecords = data;
-            this.activityRecord = JSON.parse(JSON.stringify(this.activityRecords));
-            this.activityRecord.forEach(act => {
-              act.disableButton = act.IPS_Status1__c !== 'Completed';
-            });
             this.isActivity = true;
-            this.isloading = false;
         }
        }else if(error){
            console.log('An error has ocurred');
            console.log(error);
        }
     }
-
-    handleRowAction(event) {
-        console.log('eventId: '+ event.detail.row.Id);
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-              recordId: event.detail.row.Id,
-              actionName: 'view'
-            }
-          });
-      }
 
       navigateToPage(event) {
         const page = event.target.name;
