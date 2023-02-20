0d565be8616ce9a798629aa8e7e8f3431b811dfc
annolse <ann.kristin.olsen@nav.no>
2023-02-20 21:17:31 +0100

    LWC - delmål

--- force-app/main/default/lwc/ips_myGoals/ips_myGoals.js
+++ force-app/main/default/lwc/ips_myGoals/ips_myGoals.js
@@ -3,58 +3,19 @@ import { NavigationMixin } from 'lightning/navigation';
 import Id from '@salesforce/user/Id';
 import getParticipantsGoals from '@salesforce/apex/IPS_myActivityController.getAllGoals';
 import getUserWorkTrailId from '@salesforce/apex/IPS_myWorkTrailController.getUserWorkTrailId';
+/* all logos related to IPS/UO portal */
+import IPS_HOME_LOGOS from '@salesforce/resourceUrl/ips_home_logo';
 
-const COLUMNS =[
-    {
-        label: 'Frist', 
-        fieldName: 'ActivityDate', 
-        type: 'date', hideDefaultActions: true,
-        typeAttributes:{
-            year:"numeric" ,
-            month:"numeric" ,
-            day:"2-digit" ,
-            weekday:"long"
-        }
-    },
-    {label: 'Delmål', fieldName: 'Subject',type: 'text',hideDefaultActions: true},
-    {
-        type: 'button',
-        fixedWidth: 150,
-        typeAttributes:{
-            label: 'Se detaljer',
-            title: 'Se detaljer',
-            name: 'Goal',
-            variant: 'Brand'
-        }
-    },
-    {
-        type:'button',
-        fixedWidth: 150,
-        typeAttributes:{
-            label: 'Fullført',
-            title: 'Fullført',
-            name: 'Status',
-            variant: 'Destructive',
-            disabled:{fieldName:'disableButton'}
-        }
-        
-    }
-]
 
 export default class Ips_myGoals extends NavigationMixin(LightningElement) {
-currentUser = Id;
-//currentUser ='0051x00000CiK8WAAV' ;
-goalRecords;
-@track goalRecord;
+myGoalImg = IPS_HOME_LOGOS + '/TaskFilled.svg';
+//currentUser = Id;
+currentUser ='0053O000007R0NUQA0' ;
+@track goalRecords;
 @track record;
 recordIds;
-isGoal = false;
-columns = COLUMNS;
-isloading = true;
+@track isGoal = false;
 
-get isMobile() {
-    return window.screen.width < 576;
-  }
 
 /* Fetch recordId from logged in user */
     @wire(getUserWorkTrailId,{userId: '$currentUser'})
@@ -72,29 +33,13 @@ get isMobile() {
        if(data){
         if(data.length>0){
             this.goalRecords = data;
-            this.goalRecord = JSON.parse(JSON.stringify(this.goalRecords));
-            this.goalRecord.forEach(goall => {
-              goall.disableButton = goall.Status !== 'Completed';
-            });
             this.isGoal = true;
-            this.isloading = false;
         }
        }else if(error){
            console.log('An error has ocurred');
            console.log(error);
        }
     }
-
-    handleRowAction(event) {
-        console.log('eventId: '+ event.detail.row.Id);
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-              recordId: event.detail.row.Id,
-              actionName: 'view'
-            }
-          });
-      }
 
       navigateToPage(event) {
         const page = event.target.name;
