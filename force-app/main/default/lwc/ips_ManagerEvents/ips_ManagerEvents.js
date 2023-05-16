4688854e46514e84ac51fba2f643c9313cd3478f
annolse <ann.kristin.olsen@nav.no>
2023-05-17 01:09:13 +0200

    More fixing

--- force-app/main/default/lwc/ips_ManagerEvents/ips_ManagerEvents.js
+++ force-app/main/default/lwc/ips_ManagerEvents/ips_ManagerEvents.js
@@ -1,13 +1,10 @@
 import { LightningElement ,api, wire, track} from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import Id from '@salesforce/user/Id';
+import getManagerUsers from '@salesforce/apex/IPS_ManagerEventController.getManagerUsers';
 import getEventList from '@salesforce/apex/ips_ManagerEventController.getEventsForCurrentWeek';
-import getTrailStatus from '@salesforce/apex/IPS_ManagerTrailController.getAggregatedTrailStatus';
-import getTrailJobbspesialist from '@salesforce/apex/IPS_ManagerTrailController.getAggregatedTrailJobbspesialist';
-import getManagerUsers from '@salesforce/apex/IPS_ManagerTrailController.getManagerUsers';
 import getJobCategory from '@salesforce/apex/IPS_jobController.totalJobsByCategory';
 import getJobTraining from '@salesforce/apex/IPS_jobController.totalJobTraining';
-import getEndedTrail from '@salesforce/apex/IPS_ManagerTrailController.getAggregatedEndedTrail';
 import UserNameFIELD from '@salesforce/schema/User.Name';
 
 
@@ -18,32 +15,12 @@ export default class Ips_ManagerEvents extends LightningElement {
         { label: 'Tid', fieldName: 'startTime', type: 'text'},
         { label: 'Emne', fieldName: 'subject', type: 'text' },
         { label: 'Sted', fieldName: 'location', type: 'text'},
+        { label: 'Møtetype', fieldName: 'meetingCategory', type: 'text'},
         { label: 'Deltaker', fieldName: 'participantName', type: 'text'},
         { label: 'Fødselsnr', fieldName: 'participantIdent', type: 'text' },
-        { label: 'Hjemmeadresse', fieldName: 'participantAddress', type: 'text' },
     ];
 
-    @track columnstrailStatus = [
-        { label: 'Status/fase', fieldName: 'trailStatus', type: 'text'},
-        { label: 'Antall', fieldName: 'numberOfTrail', type: 'Number' },
-    ];
 
-    @track columnstrailOwner = [
-        { label: 'Jobbspesialist', fieldName: 'employeeName', type: 'text'},
-        { label: 'Antall', fieldName: 'numberOfTrail', type: 'Number' },
-    ];
-
-    @track columnstrailEnded =[
-        { label: 'År', fieldName: 'year', type: 'text'},
-        { label: 'Måned', fieldName: 'month', type: 'text'},
-        { label: 'Årsak', fieldName: 'cause', type: 'text' },
-        { label: 'Antall', fieldName: 'numberOfTrail', type: 'Number' },
-    ];
-
-    @track columnstotalCategory =[
-        { label: 'Ansettelsesform', fieldName: 'typeOfEmployment', type: 'text'},
-        { label: 'Antall', fieldName: 'totalNumber', type: 'Number'},
-    ];
 
     defaultSortDirection = 'asc';
     sortDirection = 'asc';
@@ -51,17 +28,13 @@ export default class Ips_ManagerEvents extends LightningElement {
     @track error;
     @track eventList ;
     @track currentUserName;
-    @track trailStatusList;
-    @track trailOwnerList;
-    @track trailEndedList;
-    @track jobCategoryList;
-    @track jobTrainingList;
+    
     @track userOptions=[];
     @track value='- Alle -';
     @track optionsLoaded = false;
     initialRecordOwnerList;
     initialRecordEventList;
-    jobTrainingNumber;
+    
 
     connectedCallback(){
         getManagerUsers()
@@ -84,68 +57,20 @@ export default class Ips_ManagerEvents extends LightningElement {
         }
     }
 
+   
+
     
     /* get all events the next 7 days */
     @wire(getEventList)
     wiredAEvents({error,data}) {
         if (data) {
             this.eventList = data;
-            this.eventList = data;
         } else if (error) {
             this.error = error;
-            console.log(error);
         }
-    }
+    } 
 
-    /* get all trails pr status */
-    @wire(getTrailStatus)
-        wiredStatuses({error, data}){
-            if(data){
-                this.trailStatusList = data;
-            }else if(error){
-                this.error = error;
-            }
-        }
-
-    /* get all trails pr jobbspesialist */
-    @wire(getTrailJobbspesialist)
-        wiredOwners({error, data}){
-            if(data){
-                this.trailOwnerList = data;
-                this.initialRecordOwnerList = data;
-            }else if(error){
-                this.error = error;
-            }
-        }
-
-    /* get all ended aggregated pr year,month,cause */
-    @wire(getEndedTrail)
-        wiredEnds({error,data}){
-            if(data){
-                this.trailEndedList = data;
-            }else if(error){
-                this.error = error;
-            }  
-        }
-
-    @wire(getJobCategory)
-    wiredCategory({error,data}){
-        if(data){
-            this.jobCategoryList = data;
-        }else if(error){
-            this.error = error;
-        }  
-    }
-
-    @wire(getJobTraining)
-    wiredTraining({error,data}){
-        if(data){
-            this.jobTrainingList = data[0];
-            this.jobTrainingNumber = this.jobTrainingList?.totalNumber;
-        }else if(error){
-            this.error = error;
-        }  
-    }
+   
 
     handleUserChange(event){
         console.log(event.detail.value);
