6291cb12f89ebdb93f8279c25deac3c67e5843a4
annolse <ann.kristin.olsen@nav.no>
2024-01-18 13:13:47 +0100

    Updated versions

--- newScratch.bat
+++ newScratch.bat
@@ -5,13 +5,13 @@ call sf force:org:open --target-org %1
 
 
 echo "INSTALLERER"
-echo "Installerer platform-base 198.."
-call sf force:package:install --package 04t7U000000LPIYQA4 -r --installation-key %3 --wait 4 --publish-wait 4
+echo "Installerer platform-base 204.."
+call sf force:package:install --package 04t7U000000Y2rzQAC -r --installation-key %3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
-echo "Installerer platform-access-controll 115.."
-call sf force:package:install --package 04t7U000000Y2LoQAK -r --installation-key %3 --wait 4 --publish-wait 4
+echo "Installerer platform-access-controll 121.."
+call sf force:package:install --package 04t7U000000Y2siQAC -r --installation-key %3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
@@ -20,8 +20,8 @@ call sf force:package:install --package 04t7U000000TqvIQAS -r --installation-key
 
 echo ""
 echo "INSTALLERER"
-echo "Installerer crm-shared-timeline 1.21.0.."
-call sf force:package:install --package 04t7U0000004dytQAA -r --installation-key %3 --wait 4 --publish-wait 4
+echo "Installerer crm-shared-timeline 1.22.0.."
+call sf force:package:install --package 04t7U000000Y2OEQA0 -r --installation-key %3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
@@ -35,8 +35,8 @@ call sf force:package:install --package 04t7U000000LPPAQA4 -r --installation-key
 
 echo ""
 echo "INSTALLERER"
-echo "Installerer platform-integrasjon 102.."
-call sf force:package:install --package 04t7U000000Y25vQAC -r --installation-key %3 --wait 4 --publish-wait 4
+echo "Installerer platform-integrasjon 108.."
+call sf force:package:install --package 04t7U000000Y2t7QAC -r --installation-key %3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
@@ -50,23 +50,23 @@ call sf force:package:install --package 04t7U0000000RlpQAE -r --installation-key
 
 echo ""
 echo "INSTALLERER"
-echo "Installerer crm-platform-oppgave 0.49-0.."
-call sf force:package:install --package 04t7U000000D2SOQA0 -r --installation-key %3 --wait 4 --publish-wait 4
+echo "Installerer crm-platform-oppgave 0.50-0.."
+call sf force:package:install --package 04t7U000000Y2mGQAS -r --installation-key %3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
-echo "Installerer crm-arbeidsgiver-base 1.346.0.."
-call sf force:package:install --package 04t7U000000Y2MDQA0 -r --installation-key %3 --wait 4 --publish-wait 4
+echo "Installerer crm-arbeidsgiver-base 1.368.0.."
+call sf force:package:install --package 04t7U000000Y2nYQAS -r --installation-key %3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
-echo "Installerer platform-community-base 93."
-call sf force:package:install --package 04t7U000000Y26AQAS -r --installation-key %3 --wait 4 --publish-wait 4
+echo "Installerer platform-community-base 99."
+call sf force:package:install --package 04t7U000000Y2mpQAC -r --installation-key %3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
-echo "Installerer crm-ips 0.425.0.."
-call sf force:package:install --package 04t7U000000Y2M8QAK -r --installation-key %3 --wait 10 --publish-wait 10
+echo "Installerer crm-ips 0.435.0.."
+call sf force:package:install --package 04t7U000000Y2niQAC -r --installation-key %3 --wait 10 --publish-wait 10
 
 echo ""
 echo "TILDELER"
