1a02c48534220889b5133e016e820c79663ae2d0
annolse <ann.kristin.olsen@nav.no>
2024-06-13 15:11:05 +0200

    added participant lwc

--- force-app/main/default/lwc/ips_showParticipantDetail/ips_showParticipantDetail.js
+++ force-app/main/default/lwc/ips_showParticipantDetail/ips_showParticipantDetail.js
@@ -1,37 +1,43 @@
-import { LightningElement,track, wire,api } from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 
-const FIELDS = ["Work_Trail__c.Name", "Work_Trail__c.IPS_Participant_email_address__c", "Work_Trail__c.IPS_participants_age__c", "Work_Trail__c.IPS_Participants_home_address__c","Work_Trail__c.IPS_Partticipant_phone_number__c" ];
+const FIELDS = [
+    'Work_Trail__c.Name',
+    'Work_Trail__c.IPS_Participant_email_address__c',
+    'Work_Trail__c.IPS_participants_age__c',
+    'Work_Trail__c.IPS_Participants_home_address__c',
+    'Work_Trail__c.IPS_Partticipant_phone_number__c'
+];
 
 export default class Ips_showParticipantDetail extends LightningElement {
     //With this flag we can hide and show the Modal in html file
     @track isOpen = false;
     @api recordId;
+    @api default = false;
     detail;
 
-    @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
-    wiredRecord({ error, data }){
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    wiredRecord({ error, data }) {
         if (error) {
-
+            this.error = undefined;
         } else if (data) {
-        this.detail = data;
-        this.name = this.detail.fields.Name.value;
-        this.age = this.detail.fields.IPS_participants_age__c.value;
-        this.adress = this.detail.fields.IPS_Participants_home_address__c.value;
-        this.email = this.detail.fields.IPS_Participant_email_address__c.value;
-        this.phone = this.detail.fields.IPS_Partticipant_phone_number__c.value;
-      }
+            this.detail = data;
+            this.name = this.detail.fields.Name.value;
+            this.age = this.detail.fields.IPS_participants_age__c.value;
+            this.adress = this.detail.fields.IPS_Participants_home_address__c.value;
+            this.email = this.detail.fields.IPS_Participant_email_address__c.value;
+            this.phone = this.detail.fields.IPS_Partticipant_phone_number__c.value;
+        }
     }
-   
 
     showModal() {
-       this.isOpen = true;
+        this.isOpen = true;
     }
 
     hideModal() {
         this.isOpen = false;
     }
-    
+
     save() {
         this.isOpen = false;
     }
