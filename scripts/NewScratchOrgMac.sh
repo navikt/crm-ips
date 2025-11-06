f1fc49e21c4c3626ac21ee80041f204d3d635001
Håkon Kavli <hakon.kavli@nav.no>
2025-11-06 14:16:44 +0100

    Update sandboxscript with sf v2 commands

--- scripts/NewScratchOrgMac.sh
+++ scripts/NewScratchOrgMac.sh
@@ -7,116 +7,96 @@ sf force:org:open --target-org $1
 
 echo "INSTALLERER"
 echo "Installerer crm-platform-base 0.289.0"
-sf force:package:install --package 04tQC000000yAe1YAE -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000yAe1YAE -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer platform-access-controll 0.162.0"
-sf force:package:install --package 04tQC000000tlPhYAI -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000tlPhYAI -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-thread-view 0.7.0.."
-sf force:package:install --package 04tQC000000nfmnYAA -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000nfmnYAA -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-platform-reporting 0.44.0.." 
-sf force:package:install --package 04tQC000000xlvlYAA -r --installation-key $3 --wait 4 --publish-wait 8
+sf package install --package 04tQC000000xlvlYAA -r --installation-key $3 --wait 4 --publish-wait 8
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-shared-timeline 1.36.0"
-sf force:package:install --package 04tQC000000ynqfYAA -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000ynqfYAA -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-shared-base 1.1.0.."
- sf force:package:install --package 04t2o000000ySqpAAE -r --installation-key $3 --wait 4 --publish-wait 4
+ sf package install --package 04t2o000000ySqpAAE -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-shared-flowComponents 0.4.0.."
-sf force:package:install --package 04t7U0000008qz4QAA -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04t7U0000008qz4QAA -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-henvendelse base 0.36.0.."
-sf force:package:install --package 04tQC000000uSXtYAM -r --installation-key $3 --wait 4 --publish-wait 4
-
-echo ""
-echo "INSTALLERER"
-echo "Installerer platform-data-model  0.1.2.1"
-sf force:package:install --package 04tQC000000oHLpYAM -r --wait 4 --publish-wait 4
-
-echo ""
-echo "INSTALLERER"
-echo "Installerer custom-metadata-dao  0.1.2.1"
-sf force:package:install --package 04tQC000000oHKDYA2 -r --wait 4 --publish-wait 4
-
-echo ""
-echo "INSTALLERER"
-echo "Installerer custom-permission-helper  0.1.2.1"
-sf force:package:install --package 04tQC000000oGw2YAE -r --wait 4 --publish-wait 4
-
-echo ""
-echo "INSTALLERER"
-echo "Installerer feature-toggle  0.1.3.1"
-sf force:package:install --package 04tQC000000oHP3YAM -r --wait 4 --publish-wait 4
+sf package install --package 04tQC000000uSXtYAM -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-platform-integrasjon 0.164.0"
-sf force:package:install --package 04tQC000000zaszYAA -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000zaszYAA -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-journal-utilities 0.52.0.."
-sf force:package:install --package 04tQC000000yDDhYAM -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000yDDhYAM -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-platform-oppgave 0.65.0"
-sf force:package:install --package 04tQC000000rfOPYAY -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000rfOPYAY -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-arbeidsgiver-base 1.609.0"
-sf force:package:install --package 04tQC000000zbSTYAY -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000zbSTYAY -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-community-base 0.132.0"
-sf force:package:install --package 04tQC000000zCibYAE -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000zCibYAE -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-shared-user-notification 0.25.0"
-sf force:package:install --package 04tQC000000tKMHYA2 -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000tKMHYA2 -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-henvendelse 0.187.0"
-sf force:package:install --package 04tQC000000z8blYAA -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000z8blYAA -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "INSTALLERER"
 echo "Installerer crm-ips 0.513.0"
-sf force:package:install --package 04tQC000000zZldYAE -r --installation-key $3 --wait 4 --publish-wait 4
+sf package install --package 04tQC000000zZldYAE -r --installation-key $3 --wait 4 --publish-wait 4
 
 echo ""
 echo "TILDELER"
 echo "Tildel tilatelsessett til brukeren - IPS_management.."
-sf force:user:permset:assign --perm-set-name IPS_management
+sf org assign permset --name IPS_management
 
 echo ""
 echo "TILDELER"
 echo "Tildel tilatelsessett til brukeren IPS_Utvidet_oppf_lging_management.."
-sf force:user:permset:assign --perm-set-name IPS_Utvidet_oppf_lging_management
+sf org assign permset --name IPS_Utvidet_oppf_lging_management
 
 echo ""
 echo "TILDELER"
 echo "Tildel tilatelsessett til brukeren IPS_Config.."
-sf force:user:permset:assign --perm-set-name IPS_Config
+sf org assign permset --name IPS_Config
 
 echo "************************* FERDIG *********************************"
